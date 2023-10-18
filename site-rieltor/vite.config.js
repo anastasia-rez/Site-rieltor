5a0ba8186c0ed82d3fc6c21356cf9abd150d3b51
anastasia-rez <nastya.reznichencko21@gmail.com>
2023-10-18 17:55:27 +0300

    Move build files to root

--- site-rieltor/vite.config.js
+++ site-rieltor/vite.config.js
@@ -4,6 +4,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: '/',
   resolve: {
     alias: {
       'vue': 'vue/dist/vue.esm-bundler.js',
