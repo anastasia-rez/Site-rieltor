085cc6b19e5706534004fad74d413d0af50f83c8
anastasia-rez <nastya.reznichencko21@gmail.com>
2023-10-18 21:47:52 +0300

    return

---
+++ site-rieltor/src/components/About.vue
@@ -0,0 +1,46 @@
+<template>
+    <div id="about" class="container-xxl py-5 ">
+        <div class="container">
+            <div class="row g-5 align-items-center">
+                <div class="col-lg-6 wow fadeIn" data-wow-delay="0.1s">
+                    <div class="about-img position-relative overflow-hidden p-5 pe-0">
+                        <img class="img-fluid w-100" src="/src/assets/img/about.jpg">
+                    </div>
+                </div>
+                <div class="col-lg-6 wow fadeIn" data-wow-delay="0.5s">
+                    <h1 class="mb-4">Підберемо для Вас ідеальний варіант</h1>
+                    <p class="mb-4">Наші фахівці приймають до уваги всі деталі Вашого запросу, щоб допомогти Вам</p>
+                    <p><i class="fa fa-check text-primary me-3"></i>Великий вибір пропозицій</p>
+                    <p><i class="fa fa-check text-primary me-3"></i>Профейсійні фахівці</p>
+                    <p><i class="fa fa-check text-primary me-3"></i>Честність та бажання допомогти</p>
+                    <a class="btn py-3 px-5 mt-3" href="https://t.me/alianceexpert">Наш телеграм канал</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+
+.about-img img {
+    position: relative;
+    z-index: 2;
+}
+
+.about-img::before {
+    position: absolute;
+    content: "";
+    top: 0;
+    left: -50%;
+    width: 100%;
+    height: 100%;
+    background:  rgb(68, 68, 205);
+    transform: skew(20deg);
+    z-index: 1;
+}
+
+.btn {
+    color: white;
+    background-color:  rgb(68, 68, 205);
+}
+</style>
---
+++ site-rieltor/src/components/Search.vue
@@ -0,0 +1,74 @@
+<template>
+    <div class="container-fluid mb-5 wow fadeIn" data-wow-delay="0.1s" style="padding: 35px;">
+      <div class="container">
+        <div class="row g-2">
+          <div class="col-md-10">
+            <div class="row g-2">
+              <div class="col-md-5">
+                <select v-model="selectedService" class="form-select m-auto border-0 py-3">
+                  <option value="" selected>Тип послуги</option>
+                  <option value="rentApartments">Оренда квартир</option>
+                  <option value="sellApartments">Купівля квартири</option>
+                  <option value="rentHouses">Оренда будинка</option>
+                  <option value="sellHouses">Купівля будинка</option>
+                  <option value="landPlots">Земельні ділянки</option>
+                  <option value="offices">Офіси</option>
+                  <!-- Добавьте другие опции здесь -->
+                </select>
+              </div>
+              <div class="col-md-5">
+                <select class="form-select m-auto border-0 py-3">
+                  <option selected>Місцезнаходження</option>
+                  <option value="Dnipro">Дніпро</option>
+                  <!-- Добавьте другие опции здесь -->
+                </select>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-2 mt-2">
+            <button @click="search" class="btn border-0 w-100 py-3">Знайти</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </template>
+  
+  <script setup>
+  import { ref } from 'vue';
+  
+  const selectedService = ref('');
+  
+  const search = () => {
+    if (selectedService.value === 'rentApartments') {
+      window.location.href = 'rentApartments.html';
+    } else if (selectedService.value === 'sellApartments') {
+      window.location.href = 'sellApartments.html';
+    } else if (selectedService.value === 'rentHouses') {
+      window.location.href = 'rentHouses.html';
+    } else if (selectedService.value === 'sellHouses') {
+      window.location.href = 'sellHouses.html';
+    } else if (selectedService.value === 'landPlots') {
+      window.location.href = 'landPlots.html';
+    } else if (selectedService.value === 'offices') {
+      window.location.href = 'offices.html';
+    }
+  };
+  </script>
+  
+  <style scoped>
+  .container-fluid {
+      background-color: rgb(68, 68, 205);
+  }
+  
+  .btn {
+      background-color: rgb(36, 31, 41);
+      color: white;
+  }
+  
+  @media (max-width: 767px) {
+    select.form-select {
+      width: 230px;
+      font-size: 16px;
+    }
+  }
+  </style>
